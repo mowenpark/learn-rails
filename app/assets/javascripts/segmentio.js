d04a2ca0f9fbb8336a39a0f38361293ca99588a3
mowenpark <michaelowenpark@gmail.com>
2015-10-13 14:32:20 -0700

    analytics

--- app/assets/javascripts/segmentio.js
+++ app/assets/javascripts/segmentio.js
@@ -45,7 +45,7 @@ window.analytics.SNIPPET_VERSION = '2.0.8';
 
 // Load analytics.js with your API key, which will automatically load all of the
 // analytics integrations you've turned on for your account. Boosh!
-window.analytics.load('UA-68836905-1');
+window.analytics.load('lSKrj43cMD0DeCmdKJxV2VLORWFiLRPz');
 
 // Make our first page call to load the integrations. If you'd like to manually
 // name or tag the page, edit or move this call to use your own tags.
