0b8030765ed3e77ce2e174df81672d741fe4a9cb
mowenpark <michaelowenpark@gmail.com>
2016-04-05 17:04:33 -0700

    fix ccarousel turbolinks bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require foundation
 //= require_tree .
+//= require foundation
 $(function() {
   $(document).foundation();
 });
+//= require turbolinks
