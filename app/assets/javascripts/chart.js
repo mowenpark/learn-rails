2ed6b074eb1d86d2d5c31cb947b13bac03783dea
mowenpark <michaelowenpark@gmail.com>
2016-04-07 15:58:01 -0700

    add d3 sample chart finally

---
+++ app/assets/javascripts/chart.js
@@ -0,0 +1,10 @@
+var data = [4, 8, 15, 16, 23, 42];
+
+var chart = function () {
+  d3.select(".chart")
+    .selectAll("div")
+      .data(data)
+    .enter().append("div")
+      .style("width", function(d) { return d * 10 + "px"; })
+      .text(function(d) { return d; });
+}
