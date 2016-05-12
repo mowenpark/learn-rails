8db4502ed4941043774bae992c23e9b376b8995f
mowenpark <michaelowenpark@gmail.com>
2016-05-11 19:21:19 -0700

    render first d3 element

--- frontend/components/visone.jsx
+++ frontend/components/visone.jsx
@@ -1,24 +1,47 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
-
-var data = [4, 8, 15, 16, 23, 42];
+var d3Chart = require('../charts/d3chart');
 
 var Visone = React.createClass({
 
-  render: function() {
-    
-    d3.select(".chart")
-      .selectAll("div")
-        .data(data)
-      .enter().append("div")
-        .style("width", function(d) { return d * 10 + "px"; })
-        .text(function(d) { return d; });
+  propTypes: {
+    data: [{id: '5fbmzmtc', x: 7, y: 41, z: 6},
+          {id: 's4f8phwm', x: 11, y: 45, z: 9}],
+    domain: {x: [0, 30], y: [0, 100]}
+  },
 
+  componentDidMount: function() {
+    var el = this.getDOMNode();
+    console.log(el);
+    d3Chart.create(el, {
+      width: '100%',
+      height: '300px'
+    }, this.getChartState());
+  },
+
+  componentDidUpdate: function() {
+    var el = this.getDOMNode();
+    d3Chart.update(el, this.getChartState());
+  },
+
+  getChartState: function() {
+    return {
+      data: [{id: '5fbmzmtc', x: 7, y: 41, z: 6},
+            {id: 's4f8phwm', x: 11, y: 45, z: 9}],
+      domain: {x: [0, 30], y: [0, 100]}
+    };
+  },
+
+  componentWillUnmount: function() {
+    var el = this.getDOMNode();
+    d3Chart.destroy(el);
+  },
+
+  render: function() {
     return (
-      <div class="chart"></div>
+      <div className="Chart"></div>
     );
   }
-
 });
 
 module.exports = Visone;
--- frontend/datavis.jsx
+++ frontend/datavis.jsx
@@ -10,7 +10,18 @@ var Visone = require('./components/visone'),
     Navbar = require('./components/navbar'),
     Home = require('./components/home');
 
+var sampleData = [
+      {id: '5fbmzmtc', x: 7, y: 41, z: 6},
+      {id: 's4f8phwm', x: 11, y: 45, z: 9}]
+
 var App = React.createClass({
+
+  getInitialState: function () {
+    return {
+      data: sampleData,
+      domain: {x: [0, 30], y: [0, 100]}
+    };
+  },
 
   render: function(){
     return (
@@ -25,6 +36,7 @@ var App = React.createClass({
 var routes = (
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
+      <Route path="visone" component={Visone} />
     </Route>
 );
 
